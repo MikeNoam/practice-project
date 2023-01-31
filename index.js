b6be9ef5007b73cefb25c70f3a7134d9ae1fb7f4
Michael Harris <mike.n.harris1@gmail.com>
2023-01-31 11:50:14 -0500

    success

---
+++ index.js
@@ -0,0 +1,27 @@
+/*const h3 = document.createElement("h3");
+h3.textContent = "Hello, World!";
+
+document.querySelector("body").appendChild(h3);
+
+console.log("Hello")
+
+function sayHelloManyTimes() {
+    for (let i = 0; i < 5; i++)
+        console.log("Hello")
+        //debugger;
+}
+
+sayHelloManyTimes();
+
+const mikesArray = ["pizza", "noodles"];
+mikesArray.forEach(i => console.log(i));
+console.log ()
+*/
+
+const myObject = {
+    address: "18 Broadway",
+    city: "New York",
+    state: "NY",
+}
+
+console.log(myObj);
